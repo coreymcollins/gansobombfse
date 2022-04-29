5230c3d934ce42622769e728af1a9cf0323507b4
Corey Collins <coreymcollins@gmail.com>
2022-04-29 15:12:26 -0400

    Enqueues block styles

--- functions.php
+++ functions.php
@@ -45,3 +45,37 @@ function gansobomb_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'gansobomb_scripts' );
+
+/**
+ * Enqueues block scripts.
+ *
+ * @return void
+ * @author Corey Collins
+ */
+function gansobomb_enqueue_block_assets() {
+	$block_asset_dependencies = [ 'wp-i18n', 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ];
+	$version                  = '1.0.0';
+
+	wp_enqueue_script( 'gansobombblocks', get_stylesheet_directory_uri() . '/build/block-scripts.js', $block_asset_dependencies, $version );
+}
+add_action( 'enqueue_block_editor_assets', 'gansobomb_enqueue_block_assets' );
+
+/**
+ * Registers a block category.
+ *
+ * @param array $categories
+ * @return array
+ * @author Corey Collins
+ */
+function gansobomb_block_category( $categories ) {
+	return array_merge(
+		$categories,
+		[
+			[
+				'slug' => 'gb-blocks',
+				'title' => __( 'Ganso Bomb Blocks', 'gansobomb' ),
+			],
+		]
+	);
+}
+add_action( 'block_categories', 'gansobomb_block_category', 10, 2 );
